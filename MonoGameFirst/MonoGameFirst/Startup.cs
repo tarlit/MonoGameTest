593c0e8be24db1195336ca8a8fe68797fd9f84f7
TanyaRan <tatyanaran@gmail.com>
2016-06-04 19:10:01 +0300

    This is the initial commit

---
+++ MonoGameFirst/MonoGameFirst/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonoGameFirst
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameOne())
+                game.Run();
+        }
+    }
+#endif
+}
